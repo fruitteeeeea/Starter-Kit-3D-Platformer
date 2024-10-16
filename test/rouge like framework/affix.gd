4e1dbc30fd6001de2072f69ac52ce5c8b4867c31
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2024-10-17 01:30:12 +0800

    1017edithome
    *继续搭建肉鸽框架

--- test/rouge like framework/affix.gd
+++ test/rouge like framework/affix.gd
@@ -4,11 +4,14 @@ extends Control
 @export var enemy : Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_button_pressed() -> void:
+	player.Attribute["attack_damage"] += player.Attribute["attack_damage"] * .5
+	print(player.Attribute["attack_damage"])
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#通用属性修改函数
+func do_attribute(attribute_name : String, modifier : float) -> void:
+	if player.Attribute.has(attribute_name):
+		player.Attribute[attribute_name] += player.Attribute[attribute_name] * modifier
+		print(attribute_name + ":", player.Attribute[attribute_name])
+	else:
+		print("Attribute", attribute_name, "does not exist.")
--- test/rouge like framework/player.gd
+++ test/rouge like framework/player.gd
@@ -1,26 +1,38 @@
 extends Control
 
+#白字属性
+var attack_damage := 10.0 #白字攻击伤害
+var attack_speed := 1.2
+var crithit_rate := 0.2
+
+##首先 应当具备读取和修改里面的值
 @export var Attribute := {
-	"攻击速度" : 1.2,
-	"攻击伤害" : 10.0,
-	"移动速度" : 30.0,
-	"跳跃高度" : 20.0
+	"attack_speed" : 1.0, 
+	"attack_damage" : 1.0, #攻击伤害加成
+	"crithit_rate" : 1.0,
+	"crithit_damage" : 1.0,
+	"move_speed" : 1.0,
+	"jump_hight" : 1.0
 }
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+#临时属性
+@export var tempo_modify := {
+	"attack_speed" : 1.0, 
+	"attack_damage" : 1.0, #攻击伤害加成
+	"crithit_rate" : 1.0,
+	"crithit_damage" : 1.0,
+	"move_speed" : 1.0,
+	"jump_hight" : 1.0
+}
 
 func deal_damage(): #造成伤害
-	pass
+	var final_damage = attack_damage * Attribute["attack_damage"] #现场计算伤害结果
+	print(final_damage)
 
 
 func take_damage(): #接受伤害
 	pass
+
+
+func _on_button_pressed() -> void:
+	deal_damage()
