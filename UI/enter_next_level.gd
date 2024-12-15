e6c06278f4cfae4247dbae1054148dc0fb991b92
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2024-12-15 22:19:34 +0800

    1215edithome
    *少量修改

--- UI/enter_next_level.gd
+++ UI/enter_next_level.gd
@@ -2,9 +2,9 @@ extends Control
 
 @export var next_level : String
 
-func _on_no_pressed() -> void:
+func _on_yes_pressed() -> void:
+	LevelServer.change_scene(next_level)
 	queue_free()
 
-
-func _on_yes_pressed() -> void:
+func _on_no_pressed() -> void:
 	queue_free()
