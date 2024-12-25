fd34b0c8b8ad69edb2a94d66a687512e3b17be27
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2024-12-26 00:02:21 +0800

    1225edithome
    *继续完成 debugg 系统

--- UI/debug_panel.gd
+++ UI/debug_panel.gd
@@ -5,10 +5,27 @@ extends Control
 #
 @export var debug_info := {
 	
+
 }
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug2"):
+		visible =! visible
+		line_edit.grab_focus()
+		line_edit.select_all()
+
 func _on_line_edit_text_submitted(new_text: String) -> void:
-	print(line_edit.text)
+	match new_text:
+		"add loot":
+			LootServer.update_loot_status(1)
+			print("添加 1 个战利品")
+		"add 10xloot":
+			LootServer.update_loot_status(9, 3)
+			print("添加 1 0 个战利品")
+		
+		_:
+			print("未找到匹配方法")
+			
 	apply_debug_line()
 
 #执行操作
